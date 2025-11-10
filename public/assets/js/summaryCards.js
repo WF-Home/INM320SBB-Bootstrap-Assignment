150e3420b9e77b2fed052914a693d2841cd8dc5f
Wiljay Flores <wiljay.dev@gmail.com>
2025-11-10 04:43:26 -0500

    Added chart code

--- public/assets/js/summaryCards.js
+++ public/assets/js/summaryCards.js
@@ -1,8 +1,8 @@
 const summaryItems = [
-    { label: "unresolved", value: 60 },
-    { label: "overdue", value: 16 },
-    { label: "open", value: 43 },
-    { label: "unresolved", value: 64 }
+    { label: "Unresolved", value: 60 },
+    { label: "Overdue", value: 16 },
+    { label: "Open", value: 43 },
+    { label: "On hold", value: 64 }
 ]
 
 let summarySection = document.getElementById("summary-section");
@@ -14,7 +14,7 @@ function summaryContent(summaryItems) {
     summaryItems.forEach((item) => {
         template +=  `
         <div class="col">
-            <article class="summary-card card mb-4 rounded-3 shadow-sm py-4">
+            <article class="summary-card card rounded-3 shadow-sm py-2">
                 <h4 class="card-title">${item.label}</h4>
                 <p class="card-text">${item.value}</p>
             </article>
