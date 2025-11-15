8973e633286d3036143cc00e99c4bc0649274fc1
Wiljay Flores <wiljay.dev@gmail.com>
2025-11-15 17:47:41 -0500

    fixed fonts for unresolved tickets

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -5,16 +5,17 @@ new Chart("Chart", {
   data: {
     labels: xValues,
     datasets: [{
-      data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-      borderColor: "#DFE0EB",
-      fill: false,
-      borderWidth: 2,
-      pointRadius: 0
-    },{
       data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
       borderColor: "#3751FF",
       backgroundColor: "#3752ff0f",
       fill: true,
+      borderWidth: 2,
+      pointRadius: 0
+    },
+    {
+      data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
+      borderColor: "#DFE0EB",
+      fill: false,
       borderWidth: 2,
       pointRadius: 0
     }]
--- public/assets/js/listView.js
+++ public/assets/js/listView.js
@@ -13,9 +13,9 @@ function unresolvedTicketContent(unresolvedTickets) {
     let template = ``;
     unresolvedTickets.forEach((ticket) => {
         template +=  `
-        <li class="list-group-item d-flex flex-row justify-content-between">
-            <p>${ticket.status}</p>
-            <p>${ticket.value}</p>
+        <li class="list-group-item d-flex flex-row justify-content-between align-items-center">
+            <p class="m-0 py-2 xs-text primary-text semi-bold-text">${ticket.status}</p>
+            <p class="m-0 xs-text secondary-text semi-bold-text">${ticket.value}</p>
         </li>`
     });
     return template;
