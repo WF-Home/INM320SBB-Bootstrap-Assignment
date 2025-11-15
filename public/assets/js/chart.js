d3eadfaa67f0c719d1fee92a53c770512d9b4a97
Wiljay Flores <wiljay.dev@gmail.com>
2025-11-15 01:04:58 -0500

    refactored sidenav+ fixed font issue by overriding bootstrap native fonts

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -1,28 +1,3 @@
-// const DATA_COUNT = 22;
-// const labels = [];
-// for (let i = 0; i <= DATA_COUNT; i++) {
-//   labels.push(i.toString());
-// }
-
-// new Chart("myChart", {
-//   type: "line",
-//   data: {
-//     labels: labels,
-//     datasets: [{ 
-//       data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-//       borderColor: "green",
-//       fill: false
-//     }, { 
-//       data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-//       borderColor: "blue",
-//       fill: false
-//     }]
-//   },
-//   options: {
-//     legend: {display: false}
-//   }
-// });
-
 const xValues = [100,200,300,400,500,600,700,800,900,1000];
 
 new Chart("Chart", {
---
+++ public/assets/js/sideNav.js
@@ -0,0 +1,36 @@
+let navItems = [
+    {"label": "Overview", "imageURL": "/assets/images/sidebar/overview.svg", "isAUserSetting": false},
+    {"label": "Tickets", "imageURL": "/assets/images/sidebar/tickets.svg", "isAUserSetting": false},
+    {"label": "Ideas", "imageURL": "/assets/images/sidebar/ideas.svg", "isAUserSetting": false},
+    {"label": "Contacts", "imageURL": "/assets/images/sidebar/contacts.svg", "isAUserSetting": false},
+    {"label": "Agents", "imageURL": "/assets/images/sidebar/agents.svg", "isAUserSetting": false},
+    {"label": "Articles", "imageURL": "/assets/images/sidebar/articles.svg", "isAUserSetting": false},
+    {"label": "Settings", "imageURL": "/assets/images/sidebar/settings.svg", "isAUserSetting": true},
+    {"label": "Subscription", "imageURL": "/assets/images/sidebar/subscription.svg", "isAUserSetting": true},
+];
+
+let sidenav = document.getElementById("sidenav");
+
+sidenav.innerHTML = generateSideNav(navItems);
+
+function generateSideNav(navItems) {
+    let template = `<ul class="nav flex-column w-100">`;
+    let userSettingsTemplate = ``;
+
+    navItems.forEach((item) => {
+        if (item.isAUserSetting) {
+            userSettingsTemplate += generateListItem(item);
+        } else {
+            template += generateListItem(item);
+        }
+    })
+    template += `<hr>
+        ${userSettingsTemplate}
+    </ul>`;
+
+    return template;
+}
+
+function generateListItem(item) {
+    return `<li class="nav-item py-2"><a><img src="${item.imageURL}">${item.label}</a></li>`;
+}
